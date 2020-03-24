9b613506056f774460f6a73d518306ab9892de41
Rahul Patel <rahuljap@gmail.com>
2020-03-23 22:08:39 -0500

    git ignore and edited p1 comments

--- src/Easy/p1/Solution.java
+++ src/Easy/p1/Solution.java
@@ -4,10 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Two Sum
- * @author rahul
- *
- */
+ 
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
+
+*/
+
 public class Solution {
     // O(n^2)
     public int[] twoSum_n2(int[] nums, int target) {
